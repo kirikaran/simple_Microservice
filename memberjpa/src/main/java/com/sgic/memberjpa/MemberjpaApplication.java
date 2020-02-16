cd82591024931698c4110b1fedca3a2f39781f74
Kirikaran <kirikarankiri63@gmail.com>
2020-02-16 19:31:57 +0530

    create springboot project and intall dependencies

---
+++ memberjpa/src/main/java/com/sgic/memberjpa/MemberjpaApplication.java
@@ -0,0 +1,13 @@
+package com.sgic.memberjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberjpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemberjpaApplication.class, args);
+	}
+
+}
---
+++ memberjpa/src/test/java/com/sgic/memberjpa/MemberjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgic.memberjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberjpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
